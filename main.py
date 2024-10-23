9831bf76e93dc0cd4464b7644b73a6554109c040
Viktoria Dolgolevets <vikadolgolevec@gmail.com>
2024-10-23 21:52:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 s = input("введите строку") #ввод строки
-x= 0 
-with open("text.txt") as file:
-    file_text = file.read().splitlines()
-for string in file_text:
-    if s in string:
+x= 0 #счетчик
+with open("text.txt") as file: #открывается файл
+    file_text = file.read().splitlines() #записывается текст и разбивает на строки
+for string in file_text: #смотрит строки
+    if s in string: #если в строке есть подстрока то к счетчику +1
         print(string)
         x += 1
-print(f"колво строк с подстрокой {s}: {x}")
+print(f"колво строк с подстрокой {s}: {x}") #вывод колва
+print("отсортированные в порядке их динны: ", sorted(file_text, key = len)) #вывод отсортированных по длине строк
